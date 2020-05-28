357f0e49e326aa9f389582e1892a6168c2158a26
ChiLi-zy <1162995411@qq.com>
2020-05-28 21:46:42 +0800

    添加相关备注

--- src/main/java/com/zjc/bokeCMS/Application.java
+++ src/main/java/com/zjc/bokeCMS/Application.java
@@ -5,8 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+	//启动入口
 	public static void main(String[] args) {
+
 		SpringApplication.run(Application.class, args);
+
 	}
 
 }
